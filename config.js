bf859539b9e2b4c08ec4ef09f6e1acecc499ca23
matt.duffield <matt.duffield@gmail.com>
2017-07-17 16:37:42 -0700

    Update 2017-07-17T23:37:41.367Z

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ require.config({
     "babel": "node_modules/requirejs-babel/babel-5.8.34.min",
     //"i18next": "node_modules/i18next/dist/umd/i18next.min",
     // "breeze": "node_modules/breeze-client/build/breeze.min"
-    "split": "node_modules/split/index",
+    "split": "node_modules/split.js/split",
     "x2js": "node_modules/x2js/x2js",
   },
   packages: [
